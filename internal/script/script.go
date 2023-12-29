0c3a260fdac74504cb6608b6502685b5fad34991
Casper Eijkens <caspereijkens@icloud.com>
2023-12-29 22:49:45 +0100

    Added script parser

--- internal/script/script.go
+++ internal/script/script.go
@@ -1,1 +1,57 @@
 package script
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+type Script [][]byte
+
+// NewScript creates a new Script from a byte slice.
+func NewScript(data []byte) (Script, error) {
+	if len(data) == 0 {
+		return nil, fmt.Errorf("empty script data")
+	}
+
+	length, varintSize := binary.Uvarint(data)
+	if length <= 0 {
+		return nil, fmt.Errorf("failed to read script length")
+	}
+
+	data = data[varintSize:]
+
+	script := make(Script, 0)
+	count := 0
+
+	for count < int(length) {
+		currentByte := data[count]
+		count++
+
+		if currentByte >= 1 && currentByte <= 75 {
+			// For a number between 1 and 75 inclusive, we know the next n bytes are an element.
+			n := int(currentByte)
+			script = append(script, data[count:count+n])
+			count += n
+		} else if currentByte == 76 {
+			// 76 is OP_PUSHDATA1, so the next byte tels us how many bytes to read.
+			dataLength := int(data[count])
+			count++
+			script = append(script, data[count:count+dataLength])
+			count += dataLength
+		} else if currentByte == 77 {
+			// 77 is OP_PUSHDATA2, so the next two bytes tell us how many bytes to read.
+			dataLength := binary.LittleEndian.Uint16(data[count : count+2])
+			count += 2
+			script = append(script, data[count:count+int(dataLength)])
+			count += int(dataLength)
+		} else {
+			script = append(script, []byte{currentByte})
+		}
+	}
+
+	if count != len(data) {
+		return nil, fmt.Errorf("parsing script failed")
+	}
+
+	return script, nil
+}
---
+++ internal/script/script_test.go
@@ -0,0 +1,70 @@
+package script
+
+import (
+	"bytes"
+	"encoding/hex"
+	"reflect"
+	"testing"
+)
+
+func TestNewScript(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []byte
+		expected Script
+		wantErr  bool
+	}{
+		{
+			name:     "Empty data",
+			input:    []byte{},
+			expected: nil,
+			wantErr:  true,
+		},
+		{
+			name:     "Valid script with OP_PUSHDATA1",
+			input:    []byte{0x04, 't', 'e', 's', 't'},
+			expected: Script{[]byte{'t'}, []byte{'e'}, []byte{'s'}, []byte{'t'}},
+			wantErr:  false,
+		},
+		{
+			name:     "Valid script with OP_PUSHDATA2",
+			input:    []byte{0x04, 0x4D, 0x02, 0x00, 'a', 'b'},
+			expected: Script{[]byte{'a', 'b'}},
+			wantErr:  false,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			script, err := NewScript(tt.input)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewScript() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(script, tt.expected) {
+				t.Errorf("NewScript() got = %v, want %v", script, tt.expected)
+			}
+		})
+	}
+
+	scriptPubKeyHex := "6a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937"
+	scriptPubKey, _ := hex.DecodeString(scriptPubKeyHex)
+
+	script, err := NewScript(scriptPubKey)
+	if err != nil {
+		t.Fatalf("NewScript() error: %v", err)
+	}
+
+	wantCmd1, _ := hex.DecodeString("304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a71601")
+	if !bytes.Equal(script[0], wantCmd1) {
+		t.Errorf("Script.Parse() cmds[0] = %x, want %x", script[0], wantCmd1)
+	}
+
+	wantCmd2, _ := hex.DecodeString("035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937")
+	if !bytes.Equal(script[1], wantCmd2) {
+		t.Errorf("Script.Parse() cmds[1] = %x, want %x", script[1], wantCmd2)
+	}
+}
