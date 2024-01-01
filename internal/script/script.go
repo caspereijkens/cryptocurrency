4facfe07ba9883b3c59c23934f11bebcec5772ee
Casper Eijkens <caspereijkens@icloud.com>
2024-01-01 17:37:11 +0100

    Added Serialize method to Script type

--- internal/script/script.go
+++ internal/script/script.go
@@ -2,12 +2,15 @@ package script
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 type Script [][]byte
 
 // NewScript creates a new Script from a byte slice.
+// OP_PUSHDATA1/2 can be used to group data in a single []byte.
 func NewScript(data []byte) (Script, error) {
 	if len(data) == 0 {
 		return nil, fmt.Errorf("empty script data")
@@ -27,24 +30,25 @@ func NewScript(data []byte) (Script, error) {
 		currentByte := data[count]
 		count++
 
-		if currentByte >= 1 && currentByte <= 75 {
-			// For a number between 1 and 75 inclusive, we know the next n bytes are an element.
+		switch {
+		case currentByte >= 1 && currentByte <= 75:
+			// For a number between 1 and 75 inclusive, the next n bytes are an element.
 			n := int(currentByte)
 			script = append(script, data[count:count+n])
 			count += n
-		} else if currentByte == 76 {
-			// 76 is OP_PUSHDATA1, so the next byte tels us how many bytes to read.
+		case currentByte == 76:
+			// 76 is OP_PUSHDATA1, so the next byte tells us how many bytes to read.
 			dataLength := int(data[count])
 			count++
 			script = append(script, data[count:count+dataLength])
 			count += dataLength
-		} else if currentByte == 77 {
+		case currentByte == 77:
 			// 77 is OP_PUSHDATA2, so the next two bytes tell us how many bytes to read.
 			dataLength := binary.LittleEndian.Uint16(data[count : count+2])
 			count += 2
 			script = append(script, data[count:count+int(dataLength)])
 			count += int(dataLength)
-		} else {
+		default:
 			script = append(script, []byte{currentByte})
 		}
 	}
@@ -54,4 +58,67 @@ func NewScript(data []byte) (Script, error) {
 	}
 
 	return script, nil
+}
+
+func (s *Script) Parse(data []byte) error {
+	script, err := NewScript(data)
+	if err != nil {
+		return err
+	}
+	*s = script
+	return nil
+}
+
+func (s Script) rawSerialize() ([]byte, error) {
+	var result []byte
+
+	for _, cmd := range s {
+		length := len(cmd)
+		switch {
+		case isInteger(cmd):
+			result = append(result, cmd...)
+		case length < 75:
+			result = append(result, byte(length))
+			result = append(result, cmd...)
+		case length > 75 && length < 0x100:
+			result = append(result, 76)
+			result = append(result, byte(length))
+			result = append(result, cmd...)
+		case length >= 0x100 && length <= 520:
+			result = append(result, 77)
+			binary.LittleEndian.PutUint16(result[len(result):], uint16(length))
+			result = append(result, cmd...)
+		default:
+			return nil, errors.New("too long a cmd")
+		}
+	}
+	return result, nil
+}
+
+func isInteger(data []byte) bool {
+	// Convert []byte to string
+	strData := string(data)
+
+	// Parse the string as an integer with base 10 and bit size 64
+	_, err := strconv.ParseInt(strData, 10, 8)
+
+	// If there is no error, it is a valid integer
+	return err == nil
+}
+
+// serialize serializes the Script and adds the total length prefix.
+func (s Script) Serialize() ([]byte, error) {
+	rawResult, err := s.rawSerialize()
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the varint bytes
+	varint := make([]byte, binary.MaxVarintLen64)
+	length := binary.PutUvarint(varint, uint64(len(rawResult)))
+
+	// Append the varint and the serialized script
+	result := append(varint[:length], rawResult...)
+
+	return result, nil
 }
--- internal/script/script_test.go
+++ internal/script/script_test.go
@@ -21,15 +21,27 @@ func TestNewScript(t *testing.T) {
 			wantErr:  true,
 		},
 		{
-			name:     "Valid script with OP_PUSHDATA1",
+			name:     "Valid script with just chars",
 			input:    []byte{0x04, 't', 'e', 's', 't'},
 			expected: Script{[]byte{'t'}, []byte{'e'}, []byte{'s'}, []byte{'t'}},
+			wantErr:  false,
+		},
+		{
+			name:     "Valid script with OP_PUSHDATA1",
+			input:    []byte{0x04, 0x4C, 0x04, 't', 'e', 's', 't'},
+			expected: Script{[]byte{'t', 'e', 's', 't'}},
 			wantErr:  false,
 		},
 		{
 			name:     "Valid script with OP_PUSHDATA2",
 			input:    []byte{0x04, 0x4D, 0x02, 0x00, 'a', 'b'},
 			expected: Script{[]byte{'a', 'b'}},
+			wantErr:  false,
+		},
+		{
+			name:     "Valid script with OP_PUSHDATA2 (case showcasing pushdata2)",
+			input:    []byte{0x06, 0x4D, 0x02, 0x00, 'c', 'd', 'e'},
+			expected: Script{[]byte{'c', 'd'}, []byte{'e'}},
 			wantErr:  false,
 		},
 		// Add more test cases as needed
@@ -49,7 +61,9 @@ func TestNewScript(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestScriptParsing(t *testing.T) {
 	scriptPubKeyHex := "6a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937"
 	scriptPubKey, _ := hex.DecodeString(scriptPubKeyHex)
 
@@ -66,5 +80,27 @@ func TestNewScript(t *testing.T) {
 	wantCmd2, _ := hex.DecodeString("035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937")
 	if !bytes.Equal(script[1], wantCmd2) {
 		t.Errorf("Script.Parse() cmds[1] = %x, want %x", script[1], wantCmd2)
+	}
+}
+
+func TestSerialize(t *testing.T) {
+	want := "6a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937"
+	wantBytes, _ := hex.DecodeString(want)
+
+	var script Script
+	err := script.Parse(wantBytes)
+	if err != nil {
+		t.Errorf("Failed to parse script: %v", err)
+		return
+	}
+
+	serialized, err := script.Serialize()
+	if err != nil {
+		t.Errorf("Failed to serialize script: %v", err)
+		return
+	}
+
+	if !bytes.Equal(serialized, wantBytes) {
+		t.Errorf("Serialized result does not match. Got: %x, Want: %s", serialized, want)
 	}
 }
